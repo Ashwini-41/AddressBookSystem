646c4228676fc64a1a0dd1d6f736b44938517103
Ashwini Garad <ashwinigarad1041@gmail.com>
2024-09-03 01:59:42 +0530

    Added : Contact creation code and print it

--- absprogram/AddressBookMain.java
+++ absprogram/AddressBookMain.java
@@ -1,7 +1,35 @@
 package absprogram;
 
-public class AddressBookMain {
-        public static void main(String[] args) {
-        	System.out.println("Welcome to Address Book Program");
-        }
+import java.util.*;
+
+public class AddressBookSystem {
+private List<Contact> contacts = new ArrayList<>();
+	
+	public void addContacts(Contact contact) {
+		contacts.add(contact);
+	}
+	
+	public void printContacts() {
+		for(Contact contact: contacts) {
+			System.out.println(contact);
+		}
+	}
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book System Program");		
+		
+		AddressBookSystem addressBook = new AddressBookSystem();
+		
+		Contact contact = new Contact(
+				"Teja",
+				"Shelake",
+				"Erandwane",
+				"Pune",
+				"Maharastra",
+				"123456",
+				"1234567890",
+				"abc@gmail.com"
+				);
+		addressBook.addContacts(contact);
+		addressBook.printContacts();
+	}
 }
